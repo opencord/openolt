477ba09503f9c883b8d6fab945751419e4d167c9
Zack Williams <zdw@opennetworking.org>
2018-10-17 10:50:06 -0700

    Add versioning, test Jenkinsfile, and license headers
    Change-Id: I4d4664946b715f53f1f528d1740c0854ef154d48

--- src/translation.cc
+++ src/translation.cc
@@ -1,3 +1,20 @@
+/*
+    Copyright (C) 2018 Open Networking Foundation
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "translation.h"
 
 int interface_key_to_port_no(bcmbal_interface_key key) {
